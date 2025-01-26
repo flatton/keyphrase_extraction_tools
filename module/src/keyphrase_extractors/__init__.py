b4ce142fab834cca2baacf6744b4763e45c06e69
flatton <tomoki.fujihara@abejainc.com>
2025-01-26 16:12:31 +0900

    Update init

--- module/src/keyphrase_extractors/__init__.py
+++ module/src/keyphrase_extractors/__init__.py
@@ -1,1 +1,6 @@
+from .embedding_based import (
+    EmbeddingModel,
+    EmbeddingPrompts,
+    SentenceEmbeddingBasedExtractor,
+)
 from .graph_based_or_statistical import ClassicalExtractor
--- module/src/keyphrase_extractors/embedding_based/__init__.py
+++ module/src/keyphrase_extractors/embedding_based/__init__.py
@@ -1,1 +1,6 @@
-
+from .data import (
+    EmbeddingModel,
+    EmbeddingPrompts,
+    SentenceEmbeddingBasedExtractionConfig,
+)
+from .extractor import SentenceEmbeddingBasedExtractor
